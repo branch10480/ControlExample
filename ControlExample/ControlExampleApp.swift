6c77041c45b8a9564d46468bd75c14ab10d61b47
branch10480 <toshiharu.imaeda@gmail.com>
2024-07-28 22:28:09 +0900

    First commit

---
+++ ControlExample/ControlExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlExampleApp.swift
+//  ControlExample
+//
+//  Created by Toshiharu Imaeda on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct ControlExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ControlExampleTests/ControlExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ControlExampleTests.swift
+//  ControlExampleTests
+//
+//  Created by Toshiharu Imaeda on 2024/07/28.
+//
+
+import Testing
+@testable import ControlExample
+
+struct ControlExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
